a67d9606db244e2b614a1bc3c098b03d6a21cb68
Jose Njuguna <josenjuguna688@gmail.com>
2025-05-13 11:34:22 +0300

    Added route for application.html page

--- server.js
+++ server.js
@@ -86,6 +86,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/application", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "public", "application", "application.html")
+  );
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
